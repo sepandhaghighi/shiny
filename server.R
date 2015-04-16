b63b438a5f2464663b414a9ff05b55250dacddf0
Sepand Haghighi <sepand.haghighi@yahoo.com>
2015-04-16 18:55:14 +0430

    New Commit

--- server.R
+++ server.R
@@ -3,13 +3,23 @@ library(UsingR)
 temp<-father.son
 model<-lm(sheight~fheight,data=temp)
 estimate<-function(x){return(33.8866+0.5141*x)}
-
-
+inch2cm<-function(x){return(x*2.54)} 
+cm2inch<-function(x){return(x*0.393701)}
+plot_input<-function(data,type){
+  switch(type,
+         A=inch2cm(estimate(cm2inch(data))),
+         B=estimate(data))
+}
 shinyServer(
   function(input , output){
+  ty<-reactive(input$button)  
+ output$Predict<- renderPrint({plot_input(input$l,ty())})
+
+    output$plot1<-renderPlot({plot(temp$fheight,temp$sheight , main = " Father- Son Height Plot", xlab="Father Height" , ylab="Son Height")
+                              abline(model)
+                              })
+    output$plot2<-renderPlot({plot(model)})
     
-    output$Predict<- renderPrint({estimate(input$t)})
-    output$plot1<-renderPlot({plot(temp$fheight,temp$sheight , main = " Father- Son Height Plot", xlab="Father Height" , ylab="Son Height") })
     
   }
   
--- ui.R
+++ ui.R
@@ -1,17 +1,14 @@
 library(shiny)
 
 shinyUI(pageWithSidebar(
-  headerPanel("Son Height Prediction With Simple Linear Model"),
-  
+  headerPanel(h2("Son Height Prediction With Linear Model (By Sepand Haghighi)")),
   sidebarPanel(
-    
-    numericInput("t",'Please Enter Father Height',170,min=100 , max = 250 , step = 0.5),
+    radioButtons('button',"Please Choose",c("Centimeter"="A",
+                                            "Inch"="B")),
+    numericInput("l",'Please Enter Father Height',170,min=100 , max = 250 , step = 0.5),
     submitButton("Enter"),
-    h3("For Work With This App Only Follow This Simple 2 Step :"),
-    h5("1- Type Father Height On The TextBox"),
-    h5("2- Click On Enter")
-    
-    
+    h4("Linear Model Diagram :"),
+    plotOutput("plot2")
     
     
     ),
@@ -19,10 +16,14 @@ shinyUI(pageWithSidebar(
   mainPanel(
     
 
-    h3("Prediction Value :"),
+    h3("Prediction Value : "),
     verbatimTextOutput("Predict"),
-    plotOutput("plot1")
-
+    plotOutput("plot1"),
+    h3("For Work With This App Only Follow This Simple 2 Step:"),
+    h5("1- Type Father Height On The TextBox"),
+    h5("2- Click On Enter")
+    
+    
     
     
     )
